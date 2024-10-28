82c4483912b7b561ebddbd712a09da4717c70389
Jorge <jorestuard@gmail.com>
2024-10-28 18:59:35 +0100

    update texts

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,5 +1,3 @@
-import pkg from 'prop-types';
-const {PropTypes} = pkg;
 import { Navigation } from 'swiper/modules'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
@@ -7,17 +5,18 @@ import 'swiper/css/navigation'
 import '../styles/Slider.css'
 
 
-export const Slider = ({slides}) => {
+export const Slider = ({ slides }) => {
 
 
   return (
     <>
       <Swiper navigation={true} modules={[Navigation]}>
-      {slides.map((slide) => ( 
+        {slides.map((slide) => (
           <SwiperSlide key={slide.id}>
-            <img src={slide.image} alt={slide.name} />
+            <img loading='lazy' src={slide.image} alt={slide.name} />
             <p>{slide.quote}</p>
             <h4>{slide.name}</h4>
+            <h5>{slide.rol}</h5>
           </SwiperSlide>
         ))}
       </Swiper>
@@ -25,14 +24,3 @@ export const Slider = ({slides}) => {
   )
 }
 
-Slider.propTypes = {
-  slides: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      image: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      quote: PropTypes.string.isRequired,
-    })
-  ).isRequired,
-
-};
